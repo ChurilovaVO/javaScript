7d8a9127cf9c3c3ca160c8cc662d11d5ce65c240
Valentina <vansimova@yandex.ru>
2024-05-02 19:29:37 +0300

    Homework_3 solved

---
+++ HomeWork_3/javascript.js
@@ -0,0 +1,95 @@
+localStorage.setItem('Apple iPhone 13',JSON.stringify([
+             "Отличный телефон! Батарея держится долго.","Камера супер, фото выглядят просто потрясающе."]));
+localStorage.setItem('Samsung Galaxy Z Fold 3"',JSON.stringify([
+             "Интересный дизайн, но дорогой."]));
+localStorage.setItem('Sony PlayStation 5',JSON.stringify([
+             "Люблю играть на PS5, графика на высоте."]));
+localStorage.setItem('Телефон',JSON.stringify([]));
+
+
+const container = document.querySelector('.container');
+
+showProducts();
+
+//добавить отзыв
+const btn = document.querySelector('.btn');
+const model = document.querySelector('.model');
+const comment = document.querySelector('.comment');
+btn.addEventListener('click', function (e) {
+            let inputModel = model.value;
+            let inputComment = comment.value;
+            let arrayComments=[];
+            if (inputComment!=[] && inputModel!=null){
+            //проверяем, есть ли данный продукт в localStorage
+                if (checkProductInLocalStorage(inputModel)===true){
+                    arrayComments = JSON.parse(localStorage.getItem(inputModel));
+                    arrayComments.push(inputComment);
+                    localStorage.setItem(inputModel,JSON.stringify(arrayComments));
+                } else {
+                    arrayComments.push(inputComment);
+                    localStorage.setItem(inputModel,JSON.stringify(arrayComments));
+                }
+            } else {
+                alert("Введите продукт и ваш отзыв о нем!")
+            }
+            showProducts();
+    })
+
+//удалить отзыв
+//const removeButtonComment = document.querySelector('.removeButtonComment');
+removeButtonComment.addEventListener('click', function (e) {
+    arrayComments = JSON.parse(localStorage.getItem(removeButtonComment.name));
+    console.log(arrayComments);
+})
+
+//формирование списка продуктов, по которым есть отзывы
+function showProducts() {
+    let keys = Object.keys(localStorage);
+    container.textContent=null;
+    keys.forEach(product => {
+        if ((JSON.parse(localStorage.getItem(product))).length>0){
+            let divProduct = document.createElement('div');
+            divProduct.className='product_show';
+            divProduct.textContent = product; 
+            container.appendChild(divProduct);
+  
+            let olComments = document.createElement('ol');
+            olComments.className='comments_show';
+            divProduct.appendChild(olComments);
+            showComments(product,olComments);
+        }
+    });
+}
+
+//формирование блока отзывов под каждым продуктом
+function showComments(product, olComments) {
+    for (const comment of JSON.parse(localStorage.getItem(product))) {
+        let showComment = document.createElement('li');
+        let removeButtonComment = document.createElement('button');
+        showComment.innerHTML = comment;
+        olComments.appendChild(showComment);
+        olComments.appendChild(removeButtonComment);
+        removeButtonComment.textContent="Удалить";
+        removeButtonComment.style.margin = '10px 0px';
+        removeButtonComment.className = 'removeButtonComment';
+        removeButtonComment.name = product;
+        removeButtonComment.value=comment;
+        
+    }    
+        //olComments.style.display = 'none';
+}
+
+
+//проверка, есть ли в localStorage данный продукт
+function checkProductInLocalStorage(model) {
+    let keys = Object.keys(localStorage);
+    return keys.includes(model)?true:false;
+}
+
+//очистка хранилища
+const clear = document.querySelector('.clear');
+clear.addEventListener('click', () => {
+    localStorage.clear();
+    showProducts();
+})
+
